182dc48d07a77b175abe73c0ec93db0f26cac914
Chrystian <chrystian11.alexandre@gmail.com>
2025-08-27 10:21:00 -0300

    Pagina Dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { ProductsListComponent } from "../product/products-list/products-list.component";
+import { ProfileComponent } from "../profile/profile.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ProductsListComponent, ProductsListComponent],
+  imports: [ProductsListComponent, ProductsListComponent, ProfileComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -13,5 +14,11 @@ export class DashboardComponent {
   constructor(
     public userService: UserService
   ){}
+
+  selectedComponent: 'products' | 'profile' = 'products';
+
+  alterList(component: 'products' | 'profile'){
+    this.selectedComponent = component;
+  }
 
 }
---
+++ src/app/modules/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+}
